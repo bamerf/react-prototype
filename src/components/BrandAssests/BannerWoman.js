d24b3921954944c23ad17e8a8e42449a53411ac6
bamerf <erfanianbamdad@gmail.com>
2020-04-16 18:58:37 +1000

    Fixing BannerWoman SVG

--- src/components/BrandAssests/BannerWoman.js
+++ src/components/BrandAssests/BannerWoman.js
@@ -1,10 +1,13 @@
 import React from "react";
-import bannerWoman from "../../assets/images/bannerWoman.png";
+// import bannerWoman from "../../assets/images/bannerWoman.png";
+import { BannerWomanSvg } from "../../assets/svg/BannerWomanSvg";
+// import { ReactComponent as Woman } from "../../assets/svg/bannerWoman.svg";
 
 export default function BannerWoman({ className }) {
   return (
     <div style={{ height: 0 }}>
-      <img src={bannerWoman} alt="woman" className={className} />
+      {/* <img src={bannerWoman} alt="woman" className={className} /> */}
+      <BannerWomanSvg />
     </div>
   );
 }

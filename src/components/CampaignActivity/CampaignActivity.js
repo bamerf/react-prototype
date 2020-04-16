e6b477554df59fbe1c335c7b34b1321befa9e0f7
bamerf <erfanianbamdad@gmail.com>
2020-04-16 11:00:44 +1000

    radius2

--- src/components/CampaignActivity/CampaignActivity.js
+++ src/components/CampaignActivity/CampaignActivity.js
@@ -14,7 +14,7 @@ const useStyles = makeStyles(
     root: {
       width: "100%",
       backgroundColor: colors.white,
-      borderRadius: 4,
+      borderRadius: 2,
       boxShadow: boxShadowOn,
       overflow: "hidden",
       display: "flex",

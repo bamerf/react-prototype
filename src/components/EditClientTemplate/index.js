30467c8296278ed64775f6f5853425d002317ad9
bamerf <erfanianbamdad@gmail.com>
2020-05-06 08:11:11 +1000

    Fixed compile warning

--- src/components/EditClientTemplate/index.js
+++ src/components/EditClientTemplate/index.js
@@ -2,30 +2,27 @@ import React from "react";
 import ModalWindow from "../Modals/ModalWindow";
 import SwitchItem from "./SwitchItem";
 import SelectItem from "./SelectItem";
-import { colors } from "../../data/colors";
 import Expansion from "../Expansion/Expansion";
 
-import {
-  Typography,
-} from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 import Button from "../Button/Button";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   btnWrapper: {
     display: "flex",
     justifyContent: "flex-end",
-    marginTop: "60px"
+    marginTop: "60px",
   },
   expansionTitle: {
     color: "#F2F2F2",
   },
   green: {
-    color: "#84CD15"
+    color: "#84CD15",
   },
   black: {
-    color: "black"
-  }
+    color: "black",
+  },
 }));
 
 export default function EditClient({ open, callBack }) {
@@ -41,17 +38,21 @@ export default function EditClient({ open, callBack }) {
       <div>
         <SwitchItem
           title="Client Access"
-          description="This will allow you to restrict login access for a client. This will not restrict learners from completting lessons." />
+          description="This will allow you to restrict login access for a client. This will not restrict learners from completting lessons."
+        />
         <SwitchItem
           title="Health Check"
-          description="This will allow you to restrict login access for a client. This will not restrict learners from completting lessons." />
+          description="This will allow you to restrict login access for a client. This will not restrict learners from completting lessons."
+        />
         <SwitchItem
           title="Free Phishing Simulation"
-          description="This will allow you to restrict login access for a client. This will not restrict learners from completting lessons." />
+          description="This will allow you to restrict login access for a client. This will not restrict learners from completting lessons."
+        />
         <Expansion
           title={
             <Typography variant="body1" className={styles.expansionTitle}>
-              <span className={styles.black}>Cyber Aware</span><span className={styles.green}>Free</span>
+              <span className={styles.black}>Cyber Aware</span>
+              <span className={styles.green}>Free</span>
             </Typography>
           }
         ></Expansion>
@@ -66,12 +67,15 @@ export default function EditClient({ open, callBack }) {
         <Expansion
           title={
             <Typography variant="body1" className={styles.expansionTitle}>
-              <span className={styles.black}>Cyber Aware</span><span className={styles.green}>Free</span>
+              <span className={styles.black}>Cyber Aware</span>
+              <span className={styles.green}>Free</span>
             </Typography>
           }
         ></Expansion>
         <div className={styles.btnWrapper}>
-          <Button thick={true} kind="primary">Update Client</Button>
+          <Button thick={true} kind="primary">
+            Update Client
+          </Button>
         </div>
       </div>
     </ModalWindow>
